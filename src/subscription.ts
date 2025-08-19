a47c4f85de17973c6c7fb16c0847c0ea9e5d7239
Idrees Hassan <idrees@idreesinc.com>
2025-08-19 18:37:05 -0400

    Add rick roll event handler and migration config

--- src/subscription.ts
+++ src/subscription.ts
@@ -96,7 +96,7 @@ class VibesEventHandler implements EventHandler {
 
   matchedCount = 0
   totalPostsCounter = 0
-  settings = require(this.SETTINGS_PATH).vibesAlgo
+  settings = require(this.SETTINGS_PATH)
   secrets = require(this.SECRETS_PATH)
   keywords: string[] = []
   partialKeywords: string[] = []
@@ -104,10 +104,10 @@ class VibesEventHandler implements EventHandler {
   boostedKeywords: { [key: string]: number } = {}
   settingsLastUpdated = 0
   totalCountMetricLastUpdated = 0
-  db: Database
+  database: Database
 
-  constructor(db: Database) {
-    this.db = db
+  constructor(database: Database) {
+    this.database = database
   }
 
   async handle(evt: RepoEvent): Promise<void> {
@@ -117,6 +117,7 @@ class VibesEventHandler implements EventHandler {
     if (Date.now() - this.totalCountMetricLastUpdated > 60000) {
       this.totalCountMetricLastUpdated = Date.now()
       if (this.settings.publishMetrics !== false) {
+        // TODO: Uncomment
         // incrementMetric(
         //   this.secrets,
         //   TOTAL_POSTS_METRIC,
@@ -140,7 +141,6 @@ class VibesEventHandler implements EventHandler {
         match = match && numberOfHashtags <= 6
         let matchedKeyword: String | null = null
         if (match) {
-          // match = hasMatch(create.record.text, this.keywords, this.partialKeywords, this.negativeKeywords);
           matchedKeyword = getMatch(
             create.record.text,
             this.keywords,
@@ -182,13 +182,13 @@ class VibesEventHandler implements EventHandler {
       })
 
     if (postsToDelete.length > 0) {
-      await this.db
+      await this.database
         .deleteFrom('post')
         .where('uri', 'in', postsToDelete)
         .execute()
     }
     if (postsToCreate.length > 0) {
-      await this.db
+      await this.database
         .insertInto('post')
         .values(postsToCreate)
         .onConflict((oc) => oc.doNothing())
@@ -198,26 +198,143 @@ class VibesEventHandler implements EventHandler {
 
   async updateSettings() {
     this.settingsLastUpdated = Date.now()
-    this.settings = require(this.SETTINGS_PATH).vibesAlgo
-    this.keywords = this.settings.keywords.map((keyword: string) =>
+    this.settings = require(this.SETTINGS_PATH)
+    this.keywords = this.settings.vibesAlgo.keywords.map((keyword: string) =>
       keyword.toLowerCase(),
     )
     this.partialKeywords = this.settings.partialKeywords.map(
       (keyword: string) => keyword.toLowerCase(),
     )
-    this.negativeKeywords = this.settings.negativeKeywords.map(
-      (keyword: string) => keyword.toLowerCase(),
-    )
-    this.boostedKeywords = this.settings.boostedKeywords
+    this.negativeKeywords = [
+      ...this.settings.vibesAlgo.negativeKeywords,
+      ...this.settings.sharedNegativeKeywords,
+    ].map((keyword: string) => keyword.toLowerCase())
+    this.boostedKeywords = this.settings.vibesAlgo.boostedKeywords
     // Add boosted keywords to partial keywords
     this.partialKeywords.push(...Object.keys(this.boostedKeywords))
   }
 }
 
+class RickRollEventHandler implements EventHandler {
+  SETTINGS_PATH = './settings.json'
+  SECRETS_PATH = './secrets.json'
+
+  settings = require(this.SETTINGS_PATH)
+  secrets = require(this.SECRETS_PATH)
+  keywords: string[] = []
+  negativeKeywords: string[] = []
+  words: string[] = []
+  settingsLastUpdated = 0
+  database: Database
+
+  constructor(database: Database) {
+    this.database = database
+  }
+
+  getFirstWord(text: string): string {
+    return text
+      .trim()
+      .split(' ')[0]
+      .replaceAll(`"`, '')
+      .replaceAll(`'`, '')
+      .toLowerCase()
+  }
+
+  async handle(evt: RepoEvent): Promise<void> {
+    if (Date.now() - this.settingsLastUpdated > 10000) {
+      await this.updateSettings()
+    }
+
+    if (!isCommit(evt)) return
+    const ops = await getOpsByType(evt)
+
+    const postsToDelete = ops.posts.deletes.map((del) => del.uri)
+    const postsToCreate = ops.posts.creates
+      .filter((create) => {
+        let match =
+          !create.record.reply &&
+          (!create.record.langs || create.record.langs?.includes('en'))
+        const numberOfHashtags = (create.record.text.match(/#/g) || []).length
+        match &&= numberOfHashtags <= 6
+        if (!match) {
+          return false
+        }
+        // Check if the post content contains a whitelisted keyword
+        let matchedKeyword = getMatch(
+          create.record.text,
+          this.keywords,
+          [],
+          this.negativeKeywords,
+        )
+        if (!matchedKeyword) {
+          return false
+        }
+        // Determine if the first word matches a rick roll keyword
+        const firstWord = this.getFirstWord(create.record.text)
+        if (!this.words.includes(firstWord)) {
+          return false
+        }
+        const split = create.uri.split('/')
+        // https://github.com/bluesky-social/atproto/discussions/2523
+        const url = `https://bsky.app/profile/${split[2]}/post/${
+          split[split.length - 1]
+        }`
+        // console.log("--------------------------------------------------------");
+        console.log(url)
+        console.log(create.record.text)
+        return true
+      })
+      .map((create) => {
+        const mod = calculateMod(create.record.text, {})
+        // Map matched posts to a db row
+        console.dir(create);
+        const now = Date.now()
+        return {
+          uri: create.uri,
+          cid: create.cid,
+          first_indexed: now,
+          score: 0,
+          last_scored: 0,
+          mod: mod,
+          first_word: this.getFirstWord(create.record.text)
+        }
+      })
+
+    if (postsToDelete.length > 0) {
+      await this.database
+        .deleteFrom('rick_roll_post')
+        .where('uri', 'in', postsToDelete)
+        .execute()
+    }
+    if (postsToCreate.length > 0) {
+      await this.database
+        .insertInto('rick_roll_post')
+        .values(postsToCreate)
+        .onConflict((oc) => oc.doNothing())
+        .execute()
+    }
+  }
+
+  async updateSettings() {
+    this.settingsLastUpdated = Date.now()
+    this.settings = require(this.SETTINGS_PATH)
+    this.keywords = this.settings.rickRoll.keywords.map((keyword: string) =>
+      keyword.toLowerCase(),
+    )
+    this.negativeKeywords = [
+      ...this.settings.rickRoll.negativeKeywords,
+      ...this.settings.sharedNegativeKeywords,
+    ].map((keyword: string) => keyword.toLowerCase())
+    this.words = this.settings.rickRoll.words
+  }
+}
+
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   vibesEventHandler = new VibesEventHandler(this.db)
+  rickRollEventHandler = new RickRollEventHandler(this.db)
 
   async handleEvent(evt: RepoEvent) {
-    await this.vibesEventHandler.handle(evt)
+    // await this.vibesEventHandler.handle(evt)
+    await this.rickRollEventHandler.handle(evt)
   }
 }
